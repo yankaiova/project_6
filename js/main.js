0ae0aef757e7a29b72702105830a844387b696df
yankaiova <yanaivashka@yandex.ru>
2022-03-13 10:44:57 +0300

    joomhost

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true
+    })
+});
